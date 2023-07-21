7d2910e76be23222f0ca411b67c5e4929eff2a3d
priyanshu10 <priyanshuparate@gmail.com>
2023-07-21 20:00:14 +0530

    test fail pipeline

--- minicore-framework/src/test/java/minicore/mvc/formatters/output/JsonOutputFormatterTest.java
+++ minicore-framework/src/test/java/minicore/mvc/formatters/output/JsonOutputFormatterTest.java
@@ -5,16 +5,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.validation.constraints.AssertFalse;
+
 class JsonOutputFormatterTest {
 
     @DisplayName("Single test successful")
     @Test
     void testSingleSuccessTest() {
+
+        assertTrue(false);
         System.out.println("success");
     }
 
     @Test
-//    @Disabled("Not implemented yet")
+    // @Disabled("Not implemented yet")
     void testShowSomething() {
     }
 
